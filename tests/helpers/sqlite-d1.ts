7e51406c2b436c8becc15e337745d54a12d4ba9a
屁 <75117205+pixingzoudaiyuexing@users.noreply.github.com>
2026-09-25 00:59:48 +0800

    Crisp-12: include legacy UX migration in real SQLite D1 tests

--- tests/helpers/sqlite-d1.ts
+++ tests/helpers/sqlite-d1.ts
@@ -68,7 +68,8 @@ export class SqliteD1 {
       '0004_runtime_config.sql',
       '0005_reliability.sql',
       '0006_crisp_attachment_provider.sql',
-      '0007_crisp_upload_invites.sql'
+      '0007_crisp_upload_invites.sql',
+      '0008_crisp_legacy_ux.sql'
     ]) {
       this.exec(readFileSync(`migrations/${migration}`, 'utf8'));
     }
